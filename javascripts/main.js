6c3302fda91534e7411f920b9c317f10d86a2a4b
Larry Wright <eldub10@users.noreply.github.com>
2015-04-06 20:43:44 -0700

    added demo animation to project webpage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+
+function playAnimation() {
+	$(".animation").randomAnim({
+		duration: 6000,
+		interval: 200
+	}, function() {
+		$(".tagline").fadeIn("slow");
+	});
+}
+
+$(".animation").load(playAnimation());
+$("#replay").click(playAnimation);
